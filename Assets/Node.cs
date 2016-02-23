80c26ea1264db024e6d052d55689d02564659452
unknown <up688758@S7DEL125D-008.uni.ds.port.ac.uk>
2016-02-23 17:16:46 +0000

    First Commit

---
+++ Assets/Node.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Node{
+
+	public Waypoint waypoint;
+	public Node parent;
+
+	public float gCost;
+	public float hCost;
+	public float fCost {
+		get{
+			return gCost + hCost;
+		}
+	}
+
+	public Node(Waypoint _waypoint, Node _parent, float _gCost, float _hCost){
+		_waypoint = waypoint;
+		_parent = parent;
+		_gCost = gCost;
+		_hCost = hCost;
+	}
+}
---
+++ Assets/Raycasttest.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Raycasttest : MonoBehaviour {
+	public Waypoint[] objects;
+
+	Ray ray;
+	RaycastHit hit;
+	Node StartNode;
+	Node EndNode;
+
+	// Use this for initialization
+	void Start () {
+		//objects.Add(
+		buildGraph ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void buildGraph () {
+		objects = GameObject.FindObjectsOfType<Waypoint>();
+		for (int i = 0; i < objects.Length; i++) {
+			for (int y = 0; y < objects.Length; y++) {
+				Ray ray = new Ray (objects [y].transform.position, (objects [i].transform.position - objects [y].transform.position));
+				//Physics.Raycast (objects [0].transform.position, (objects [1].transform.position - objects [0].transform.position));
+				
+				if (i != y && Physics.Raycast (ray) == false) {
+					objects[i].neighbours.Add(objects[y]);
+					//Vector3.Distance(objects [y].transform.position, objects [0].transform.position);
+					Debug.DrawRay (objects [y].transform.position, (objects [i].transform.position - objects [y].transform.position), Color.black, Mathf.Infinity);
+				}
+			}
+		}
+	}
+
+	void aStar (Node start, Node end) {
+		List<Node> openList = new List<Node>();
+		List<Node> closedList = new List<Node>();
+		Node currentNode = new Node (start.waypoint, null, 0, Vector3.Distance (start.waypoint.transform.position, end.waypoint.transform.position));
+
+
+		openList.Add (currentNode);
+
+		while(currentNode != end || openList != null){
+			for(int y = 0; y <= openList.Count; y++){
+				if(openList[y].fCost < currentNode.fCost || (openList[y].fCost == currentNode.fCost && openList[y].hCost < currentNode.hCost)){
+					closedList.Add(openList[y]);
+				}
+			}
+			for(int i = 0; i <= currentNode.waypoint.neighbours.Count; i++){
+				for(int z = 0; z < closedList.Count; z++)
+				{
+					if(closedList[z].waypoint.neighbours.Contains(currentNode.waypoint.neighbours[i])){
+						i++;
+						Debug.Log("skipped");
+						}
+					else{
+						openList.Add(currentNode.waypoint.neighbours[i]);
+
+					} 
+				}
+			}
+		}
+
+	}
+
+}
+
