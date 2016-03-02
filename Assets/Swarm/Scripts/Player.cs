1ff904d371ec9280b086481be03166d39c3df42d
thecyidoniac <thecyidoniac@gmail.com>
2016-03-02 07:49:20 +0000

    Changed Movement, Added mouse turning.

--- Assets/Swarm/Scripts/Player.cs
+++ Assets/Swarm/Scripts/Player.cs
@@ -5,30 +5,52 @@ public class Player : MonoBehaviour {
 
 	public float Speed = 0.2f;
 	public int Health = 100;
+	public float bulletSpeed = 20.0f;
 
 	void movement (){
 		Vector3 tempPos = transform.position;
 
 		if (Input.GetButton ("Forward")) {
-			tempPos.z += Speed * Time.deltaTime;
+			tempPos += (-transform.forward * Speed) * Time.deltaTime;
 		}
 
 		if (Input.GetButton ("Backward")) {
-			tempPos.z -= Speed * Time.deltaTime;
+			tempPos -= (transform.forward * Speed) * Time.deltaTime;
 		}
-
 		if(Input.GetButton("Right")){
-			tempPos.x += Speed * Time.deltaTime;
+			tempPos += (transform.right * Speed) * Time.deltaTime;
 		}
-
 		if(Input.GetButton("Left")){
-			tempPos.x -= Speed * Time.deltaTime;
+			tempPos -= (-transform.right * Speed) * Time.deltaTime;
 		}
-
 		transform.position = tempPos;
+	}
+
+	void lookat (){
+
+		Vector3 lookAt = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, (transform.position - Camera.main.transform.position).magnitude));
+		lookAt.y = transform.position.y;  // Set it to the players height to avoid any non-y rotations
+		transform.LookAt(lookAt);
+
+	}
+
+	void onShoot(){
+		Instantiate (GameObject.Find("Bullet"));
+		Vector3 tempBulPos = new Vector3();
+		tempBulPos = transform.position;
+		tempBulPos += transform.forward * bulletSpeed;
+	}
+
+
+	void Start(){
 	}
 
 	void Update(){
 		movement ();
+		lookat ();
+		if (Input.GetButtonDown ("Fire1")) {
+			onShoot();
+		}
+
 	}
 }
--- Assets/Swarm/Scripts/Shooting.cs
+++ Assets/Swarm/Scripts/Shooting.cs
@@ -5,27 +5,31 @@ public class Shooting : MonoBehaviour {
 	 
 	float bulletSpeed = 20.0f;
 
-
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	void onFire(){
+		Instantiate (gameObject);
+		Vector3 tempPos = new Vector3();
+		tempPos = transform.position;
+		tempPos += transform.forward * bulletSpeed;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton ("Fire1")){
-			RaycastHit hit;
+		if (Input.GetButtonDown ("Fire1")) {
+			onFire();
+		}
+			/*RaycastHit hit;
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if( Physics.Raycast(ray, out hit))
 			{
-				Debug.Log("HELP");
 				Vector3 newpoint = hit.point;
 				Debug.DrawLine(transform.position, newpoint, Color.red);
 				transform.position = Vector3.MoveTowards(transform.position, newpoint, Time.deltaTime * bulletSpeed);
-				//transform.position = Vector3.MoveTowards(transform.position, newpoint, Time.deltaTime * bulletSpeed);
-
-			}
-
+				}*/
 		}
-	}
 }
+
